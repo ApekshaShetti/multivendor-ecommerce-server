539251c7098ad2a5d78871f16df044e179ce43b4
Apeksha <apekshashetti1012@gmail.com>
2023-09-02 22:17:14 +0530

    Vendor mdels created, API created, Url created

--- ecommerce_app/models.py
+++ ecommerce_app/models.py
@@ -5,3 +5,23 @@ from django.contrib.auth.models import User
 class Vendor(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     address = models.TextField(null=True,blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+# Product Category
+class ProductCategory(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True,blank=True)
+
+    def __str__(self):
+        return self.title
+    
+# Products
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True,blank=True)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.title
--- ecommerce_app/views.py
+++ ecommerce_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from . import serializers
+from rest_framework import generics,permissions
+from . import models
 
-# Create your views here.
+# ListAPIView returns data in a list
+class VendorList(generics.ListCreateAPIView):
+    queryset = models.Vendor.objects.all()
+    serializer_class = serializers.VendorSerializer
+    # permission_classes = [permissions.IsAuthenticated]    # view level permission
+
+class VendorDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Vendor.objects.all()
+    serializer_class = serializers.VendorDetailSerializer
+    # permission_classes = [permissions.IsAuthenticated]    # view level permission
+
