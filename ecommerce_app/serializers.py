ee9b1c7edd3f5b85c057b45a4068bb2f3c24ca22
Apeksha <apekshashetti1012@gmail.com>
2023-09-03 18:53:05 +0530

    customer and order model complete

--- ecommerce_app/serializers.py
+++ ecommerce_app/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from . import models
 
+ # vendor
 class VendorSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Vendor
@@ -8,7 +9,7 @@ class VendorSerializer(serializers.ModelSerializer):
 
     def __init__(self, *args, **kwargs):
         super(VendorSerializer,self).__init__(*args,**kwargs)
-        self.Meta.depth = 1
+        # self.Meta.depth = 1
 
 class VendorDetailSerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,8 +18,9 @@ class VendorDetailSerializer(serializers.ModelSerializer):
 
     def __init__(self, *args, **kwargs):
         super(VendorDetailSerializer,self).__init__(*args,**kwargs)
-        self.Meta.depth = 1
+        # self.Meta.depth = 1
 
+ # product
 class ProductListSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Product
@@ -26,7 +28,7 @@ class ProductListSerializer(serializers.ModelSerializer):
 
     def __init__(self, *args, **kwargs):
         super(ProductListSerializer,self).__init__(*args,**kwargs)
-        self.Meta.depth = 1
+        # self.Meta.depth = 1
 
 
 class ProductDetailSerializer(serializers.ModelSerializer):
@@ -36,4 +38,44 @@ class ProductDetailSerializer(serializers.ModelSerializer):
 
     def __init__(self, *args, **kwargs):
         super(ProductDetailSerializer,self).__init__(*args,**kwargs)
+        # self.Meta.depth = 1
+
+# Customer
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Customer
+        fields=['id','user','mobile']
+
+    def __init__(self, *args, **kwargs):
+        super(CustomerSerializer,self).__init__(*args,**kwargs)
+        self.Meta.depth = 1
+
+class CustomerDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Customer
+        fields=['id','user','mobile']
+
+    def __init__(self, *args, **kwargs):
+        super(CustomerDetailSerializer,self).__init__(*args,**kwargs)
+        self.Meta.depth = 1
+
+# Order
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Order
+        fields=['id','customer','order_time']
+
+    def __init__(self, *args, **kwargs):
+        super(OrderSerializer,self).__init__(*args,**kwargs)
+        self.Meta.depth = 1
+
+
+class OrderDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.OrderItems
+        fields=['id','order','product']
+
+    def __init__(self, *args, **kwargs):
+        super(OrderDetailSerializer,self).__init__(*args,**kwargs)
         self.Meta.depth = 1
--- ecommerce_app/views.py
+++ ecommerce_app/views.py
@@ -4,6 +4,7 @@ from rest_framework import generics,permissions
 from . import models
 
 # ListAPIView returns data in a list
+ # vendor
 class VendorList(generics.ListCreateAPIView):
     queryset = models.Vendor.objects.all()
     serializer_class = serializers.VendorSerializer
@@ -14,7 +15,7 @@ class VendorDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = serializers.VendorDetailSerializer
     # permission_classes = [permissions.IsAuthenticated]    # view level permission
 
-
+ # product
 class ProductList(generics.ListCreateAPIView):
     queryset = models.Product.objects.all()
     serializer_class = serializers.ProductListSerializer
@@ -23,5 +24,28 @@ class ProductList(generics.ListCreateAPIView):
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = models.Product.objects.all()
     serializer_class = serializers.ProductDetailSerializer
+    # permission_classes = [permissions.IsAuthenticated]    # view level permission
+
+# Customer
+
+class CustomerList(generics.ListCreateAPIView):
+    queryset = models.Customer.objects.all()
+    serializer_class = serializers.CustomerSerializer
+    # permission_classes = [permissions.IsAuthenticated]    # view level permission
+
+class CustomerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Customer.objects.all()
+    serializer_class = serializers.CustomerDetailSerializer
+    # permission_classes = [permissions.IsAuthenticated]    # view level permission
+
+# Order
+class OrderList(generics.ListCreateAPIView):
+    queryset = models.Order.objects.all()
+    serializer_class = serializers.OrderSerializer
+    # permission_classes = [permissions.IsAuthenticated]    # view level permission
+
+class OrderDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.OrderItems.objects.all()
+    serializer_class = serializers.OrderDetailSerializer
     # permission_classes = [permissions.IsAuthenticated]    # view level permission
 
